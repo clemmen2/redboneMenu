efdd7066fc78457f9daf050f591b3d918f098714
Will Dixon <dixonwille@hotmail.com>
2015-03-04 20:47:13 -0500

    Removed Window Frame and Implimented Window Functions

--- src/js/app.js
+++ src/js/app.js
@@ -14,4 +14,28 @@ var setMenuApp = angular.module('setMenuApp', ['ngRoute'])
 			.when('/', {
 				redirectTo: '/create'
 			})
+	})
+
+	.controller('windowManagement', function(){
+		var gui = require('nw.gui');
+		var win = gui.Window.get();
+		win.isMax = false;
+		this.winMin = function() {
+			win.minimize();
+		};
+		this.winClose = function() {
+			win.close();
+		};
+		this.winMax = function() {
+			if (win.isMax)
+				win.unmaximize();
+			else
+				win.maximize();
+		};
+		win.on('maximize', function(){
+			win.isMax = true;
+		});
+		win.on('unmaximize', function(){
+			win.isMax = false;
+		});
 	});
