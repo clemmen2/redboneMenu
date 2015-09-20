1268aeb1867f4be2958a859d3a04fb8590c6f563
Will Dixon <dixonwille@hotmail.com>
2015-09-20 09:27:33 -0400

    Made PDF Pretty

--- RedBoneApp/js/factories/makePdfFact.js
+++ RedBoneApp/js/factories/makePdfFact.js
@@ -32,15 +32,43 @@
                                 fs.mkdirSync(path.parse(filePath).dir);
                                 createPdf();
                             } else {
+                                var mainCats = [];
+                                menuItems.map(function (curItem) {
+                                    if (mainCats.length == 0) {
+                                        mainCats.push(curItem.category.mainCat._id);
+                                    } else if (mainCats.indexOf(curItem.category.mainCat._id) == -1){
+                                        mainCats.push(curItem.category.mainCat._id);
+                                    }
+                                });
+                                var modSize = 0;
+                                var modSpaces = mainCats.length;
+                                if (menuItems.length <= 3) {
+                                    modSize = 10;
+                                    modSpaces = modSpaces + 3;
+                                } else if (menuItems.length == 4) {
+                                    modSize = 6;
+                                    modSpaces = modSpaces + 2;
+                                } else if (menuItems.length <= 8) {
+                                    modSize = 4;
+                                    modSpaces = modSpaces + 1;
+                                    if (mainCats.length > 1) {
+                                        modSize = modSize - 1;
+                                        modSpaces = modSpaces - 1;
+                                    }
+                                }
+                                var neededSpaces = (9 - menuItems.length) - modSpaces;
                                 var writeStream = fs.createWriteStream(filePath);
                                 doc.pipe(writeStream);
-                                doc.fontSize(25)
+                                doc.roundedRect(50,50,510,700,10)
+                                    .stroke();
+                                doc.fontSize(25 + modSize)
                                     .text("Welcom To Red Bone Alley", { align: 'center' })
                                     .fontSize(12)
+                                    .moveDown()
                                     .moveDown();
                                 var mainCatIdsUsed = [];
                                 menuItems.map(function (curItem) {
-                                    tempCat = mainCatIdsUsed.filter(function (curCat) {
+                                    var tempCat = mainCatIdsUsed.filter(function (curCat) {
                                         if (curItem.category.mainCat._id == curCat._id) {
                                             return true;
                                         } else {
@@ -49,57 +77,75 @@
                                     });
                                     if (tempCat.length !== 0) {
                                         if (usePrice && (time == "Dinner" || curItem.lunchPrice == "")) {
-                                            doc.fontSize(16)
+                                            doc.fontSize(16 + modSize)
                                                 .text(curItem.name + '    ' + curItem.price, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .text(curItem.desc, { align: 'center' })
                                                 .moveDown();
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
                                         } else if (usePrice && curItem.lunchPrice != ""){
-                                            doc.fontSize(16)
+                                            doc.fontSize(16 + modSize)
                                                 .text(curItem.name + '    ' + curItem.lunchPrice, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .text(curItem.desc, { align: 'center' })
                                                 .moveDown();
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
                                         } else {
-                                            doc.fontSize(16)
+                                            doc.fontSize(16 + modSize)
                                                 .text(curItem.name, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .text(curItem.desc, { align: 'center' })
                                                 .moveDown();
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
                                         }
                                     } else {
                                         if (usePrice && (time == "Dinner" || curItem.lunchPrice == "")) {
-                                            doc.fontSize(20)
+                                            doc.fontSize(20 + modSize)
                                                 .text(curItem.category.mainCat.name, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .moveDown();
-                                            doc.fontSize(16)
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
+                                            doc.fontSize(16 + modSize)
                                                 .text(curItem.name + '    ' + curItem.price, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .text(curItem.desc, { align: 'center' })
                                                 .moveDown();
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
                                             mainCatIdsUsed.push(curItem.category.mainCat);
                                         } else if (usePrice && curItem.lunchPrice != "") {
-                                            doc.fontSize(20)
+                                            doc.fontSize(20 + modSize)
                                                 .text(curItem.category.mainCat.name, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .moveDown();
-                                            doc.fontSize(16)
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
+                                            doc.fontSize(16 + modSize)
                                                 .text(curItem.name + '    ' + curItem.lunchPrice, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .text(curItem.desc, { align: 'center' })
                                                 .moveDown();
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
                                             mainCatIdsUsed.push(curItem.category.mainCat);
                                         } else {
-                                            doc.fontSize(20)
+                                            doc.fontSize(20 + modSize)
                                                 .text(curItem.category.mainCat.name, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .moveDown();
-                                            doc.fontSize(16)
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
+                                            doc.fontSize(16 + modSize)
                                                 .text(curItem.name, { align: 'center' })
-                                                .fontSize(12)
+                                                .fontSize(12 + modSize)
                                                 .text(curItem.desc, { align: 'center' })
                                                 .moveDown();
+                                            for (var i = 0; i < neededSpaces; i++)
+                                                doc.moveDown();
                                             mainCatIdsUsed.push(curItem.category.mainCat);
                                         }
                                     }
