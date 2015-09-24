a894e088872c8c51a3d0604444e7e4a8076470c3
Will Dixon <dixonwille@hotmail.com>
2015-09-24 19:06:20 -0400

    used $window.confirm

--- RedBoneApp/js/controllers/treeCtrl.js
+++ RedBoneApp/js/controllers/treeCtrl.js
@@ -1,8 +1,8 @@
 ﻿(function () {
     angular.module('rbApp')
         .controller('treeCtrl', treeCtrl);
-    treeCtrl.$inject = ['$filter', '$routeParams', 'categoryFact', 'itemFact', 'treeFact'];
-    function treeCtrl($filter, $routeParams, categoryFact, itemFact, treeFact) {
+    treeCtrl.$inject = ['$filter', '$routeParams', '$window', 'categoryFact', 'itemFact', 'treeFact'];
+    function treeCtrl($filter, $routeParams, $window, categoryFact, itemFact, treeFact) {
         var vm = this;
         categoryFact.getCatsDB(function (cats) {
             vm.cats = $filter('orderBy')(cats, 'mainCat.pos');
@@ -12,7 +12,7 @@
         });
         vm.catId = $routeParams.catId;
         vm.removeItem = function (id) {
-            if (confirm('Are you sure you want to delete this item?'))
+            if ($window.confirm('Are you sure you want to delete this item?'))
                 itemFact.removeItem(id);
             itemFact.getItemsDB(function (items) {
                 vm.items = items;
